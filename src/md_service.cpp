5d8622e51f75f4091854d69651186334e315d293
yangyang <yangyang@shinnytech.com>
2018-08-28 09:42:27 +0800

    修正tcp参数设置错误

--- src/md_service.cpp
+++ src/md_service.cpp
@@ -320,8 +320,8 @@ bool Init()
     info.gid = -1;
     info.uid = -1;
     info.extensions = exts;
-    info.ka_time = 15000;
-    info.ka_interval = 3000;
+    info.ka_time = 15;
+    info.ka_interval = 3;
     info.ka_probes = 5;
     info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
     md_context.m_ws_context = lws_create_context(&info);
