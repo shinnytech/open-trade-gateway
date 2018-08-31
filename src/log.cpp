f4d181006c3762e0d6c4eb62244df331a065a913
yangyang <yangyang@shinnytech.com>
2018-08-31 14:31:37 +0800

    修正log中年份错误bug

--- src/log.cpp
+++ src/log.cpp
@@ -37,7 +37,7 @@ const char* CurrentDateTimeStr()
     std::time_t t = s.count();
     std::tm* tm = std::localtime(&t);
     sprintf(dtstr, "%04d-%02d-%02dT%02d:%02d:%02d.%09lld+08:00"
-    , tm->tm_year, tm->tm_mon + 1, tm->tm_mday
+    , tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday
     , tm->tm_hour, tm->tm_min, tm->tm_sec
     , fractional_seconds);
     return dtstr;
